897529f6ebd1cdd1634c8ca7308c5688078a2177
jspfei <jiangfeifan8@163.com>
2019-01-04 09:50:26 +0800

    hh

--- src/pages/form/Form.vue
+++ src/pages/form/Form.vue
@@ -21,6 +21,67 @@
           <input type="checkbox" id="checkbox" v-model="checked">
           <label for="checkbox">{{checked}}</label>
       </div>
+      <div>
+         <h4>单选按钮</h4>
+         <input type="radio" id="one" value="One" v-model="picked">
+         <label for="one">One</label>
+        <br>
+        <input type="radio" id="two" value="Two" v-model="picked">
+        <label for="two">Two</label>
+        <br>
+        <span>Picked:{{ picked }}</span>
+      </div>
+      <div>
+         <h4>选择框</h4>
+          <h5>单选时</h5>
+          <select v-model="selected">
+              <option disabled value="">请选择</option>
+              <option>A</option>
+               <option>B</option>
+               <option>C</option>
+          </select>
+
+          <span>Selected:{{ selected}}</span>
+          <br>
+           <select v-model="selected">
+              <option v-for="option in options" v-bind:value="option.value">
+                 {{option.text}}
+              </option>
+           </select>
+         <span>Selected: {{ selected }}</span>
+        <br>
+        <h5>多选时</h5>
+         <select v-model="selecteds" multiple style="width:50px">
+           <option>A</option>
+           <option>B</option>
+           <option>C</option>
+         </select>
+        <span>Selected:{{ selecteds}}</span>
+      </div>
+    </div>
+    <div class="box">
+      <h3>值绑定</h3>
+      <!-- 当选中时，`picked` 为字符串 "a" -->
+      <input type="radio" v-model="picked" value="a">
+
+      <!-- `toggle` 为 true 或 false -->
+      <input type="checkbox" v-model="toggle">
+
+      <!-- 当选中第一个选项时，`selected` 为字符串 "abc" -->
+      <select v-model="selected">
+        <option value="abc">ABC</option>
+      </select>
+      <h4>多选框</h4>
+      <input
+        type="checkbox"
+        v-model="toggle"
+        true-value="yes"
+        false-value="no"
+      >
+      <h4>单选按钮</h4>
+      <input v-model.lazy="msg" >
+      <input v-model.number="age" type="number">
+      <input v-model.trim="msg">
     </div>
   </div>
 </template>
@@ -31,7 +92,16 @@
     data(){
       return {
         msg:"表单输入绑定",
-        message:""
+        message:"",
+        picked:"",
+        selected:"A",
+        selecteds:[],
+        pick:"a",
+        options: [
+          { text: 'One', value: 'A' },
+          { text: 'Two', value: 'B' },
+          { text: 'Three', value: 'C' }
+        ]
       }
     }
   }
